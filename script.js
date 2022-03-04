a70ab78d0e976e77af624ba6a43d5ea184db8d70
Yan Carlos (Koda) <78356819+K0dax@users.noreply.github.com>
2022-03-04 13:12:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+const fields = document.querySelectorAll("[required]")
+
+function ValidateField(field) {
+    // logica para verificar se existem erros
+    function verifyErrors() {
+        let foundError = false;
+
+        for(let error in field.validity) {
+            // se não for customError
+            // então verifica se tem erro
+            if (field.validity[error] && !field.validity.valid ) {
+                foundError = error
+            }
+        }
+        return foundError;
+    }
+
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Por favor, preencha este campo"
+            },
+            email: {
+                valueMissing: "Email é obrigatório",
+                typeMismatch: "Por favor, preencha um email válido"
+            }
+        }
+
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector("span.error")
+        
+        if (message) {
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+        } else {
+            spanError.classList.remove("active")
+            spanError.innerHTML = ""
+        }
+    }
+
+    return function() {
+
+        const error = verifyErrors()
+
+        if(error) {
+            const message = customMessage(error)
+
+            field.style.borderColor = "red"
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor = "green"
+            setCustomMessage()
+        }
+    }
+}
+
+
+function customValidation(event) {
+
+    const field = event.target
+    const validation = ValidateField(field)
+
+    validation()
+
+}
+
+for( field of fields ){
+    field.addEventListener("invalid", event => { 
+        // eliminar o bubble
+        event.preventDefault()
+
+        customValidation(event)
+    })
+    field.addEventListener("blur", customValidation)
+}
+
+
+document.querySelector("form")
+.addEventListener("submit", event => {
+    console.log("enviar o formulário")
+
+    // não vai enviar o formulário
+    event.preventDefault()
+})
+
+document.querySelector("#qtde").addEventListener("change", att)
+document.querySelector("#qtde1").addEventListener("change", att)
+document.querySelector("#qtde2").addEventListener("change", att)
+
+function att(){
+    const qtde = document.querySelector("#qtde").value
+    const qtde1 = document.querySelector("#qtde1").value
+    const qtde2 = document.querySelector("#qtde2").value
+  
+    let preco = qtde * 10;
+    let preco1 = qtde1 * 20;
+    let preco2 = qtde2 * 30;
+    precototal = preco + preco1 + preco2;
+   
+    document.querySelector("#precot").innerHTML = `R$ ${precototal.toFixed(2)}`
+}
